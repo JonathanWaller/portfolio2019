6912e76585e59c06bd9d5fd68a334222fd27cd55
JonathanWaller <waller.j27@gmail.com>
2019-06-05 23:15:19 -0500

    configured server for hosting

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const path = require('path')
+require('dotenv').config();
+const express = require('express');
+const { json } = require('body-parser');
+
+const port = 3005;
+
+//initialize express
+const app = express();
+
+//express static build
+app.use(express.static(__dirname + '/assets'));
+app.use(express.static(`${__dirname}/../build`));
+app.use(express.static(__dirname + '/assets'));
+
+//jw build
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../build/index.html"));
+// });
+
+//body-parser
+app.use(json());
+
+app.listen(port, () => {
+    console.log(`Portfolio server listening on port ${port}`);
+});

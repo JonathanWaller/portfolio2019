164a68092c35bc15eab7e46965ac698918475f52
JonathanWaller <waller.j27@gmail.com>
2020-03-01 20:00:39 -0600

    updated index for build

--- server/index.js
+++ server/index.js
@@ -8,18 +8,18 @@ const port = 3008;
 //initialize express
 const app = express();
 
+//body-parser
+app.use(json());
+
 //express static build
-app.use(express.static(__dirname + '/assets'));
+// app.use(express.static(__dirname + '/assets'));
 app.use(express.static(`${__dirname}/../build`));
 app.use(express.static(__dirname + '/assets'));
 
 //jw build
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../build/index.html"));
-// });
-
-//body-parser
-app.use(json());
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Portfolio server listening on port ${port}`);
